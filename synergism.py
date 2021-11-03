1aaada75af0894a4d905602cbbf47c83bfed66e9
Michael Otty <michael.otty@gmail.com>
2021-11-03 14:01:10 +0000

    Handle all runtime exceptions

--- synergism.py
+++ synergism.py
@@ -1,11 +1,13 @@
 """Reads a synergism save file and displays contents in a GUI"""
 
 import base64
+import binascii
 import json
 import tkinter as tk
 import uuid
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
 
 
 class App(tk.Tk):
@@ -15,32 +17,46 @@ class App(tk.Tk):
         """Constructor"""
         super().__init__()
 
-        self.file_contents = 0
+        self.file_contents = {}
 
         self.title('Synergism Reader')
 
-        self.btn_choose_file = ttk.Button(
-            self, text='Open a Synergism Save File...', command=self.open_file)
         self.tree = ttk.Treeview(self, columns='Values', height=40)
-        self.tree.column('Values', width=500, anchor='center')
+        self.tree.column('0', width=300)
+        self.tree.column('Values', width=300, anchor='center')
         self.tree.heading('Values', text='Values')
 
-        self.scroll_bar = ttk.Scrollbar(self, orient='vertical', command=self.tree.yview)
+        self.scroll_bar = ttk.Scrollbar(
+            self, orient='vertical', command=self.tree.yview)
         self.tree.configure(yscrollcommand=self.scroll_bar.set)
 
-        self.tree.grid(row=0, column=0, pady=2)
-        self.scroll_bar.grid(row=0, column=1)
-        self.btn_choose_file.grid(row=1, column=0, columnspan=2, pady=2)
+        self.tree.pack(side='left')
+        self.scroll_bar.pack(side='left', fill='y')
 
-    def open_file(self):
+        self._open_file()
+
+    def _open_file(self):
         """Opens a file picker and read file"""
         file_name = askopenfilename(
-            title='Choose a save file...', filetypes=(('Synergism Save', '.txt'),))
+            title='Choose a Synergism save file...', filetypes=(('Synergism Save', '.txt'),))
 
-        with open(file_name, 'r', encoding='utf-8') as file:
-            encoded_file = file.read()
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                encoded_file = file.read()
+        except FileNotFoundError:
+            return
 
-        self.file_contents = json.loads(base64.b64decode(encoded_file))
+        try:
+            self.file_contents = json.loads(
+                base64.b64decode(encoded_file, validate=True))
+        except binascii.Error:
+            showerror(title='Invalid File!',
+                      message='The chosen file is an invalid save file. Please choose another one!')
+            self._open_file()
+            return
+
+        # Clear then re-populate the Treeview
+        self.tree.delete(*self.tree.get_children())
         self._dict_to_tree('', self.file_contents)
 
     def _dict_to_tree(self, parent, dictionary):
